ddf4262089d598981d8a0229a30b1849fefc2748
Vignesh Sadhu <vigneshsadhu143@gmail.com>
2024-03-20 21:20:08 +0530

    register controller

--- src/models/user.modal.js
+++ src/models/user.modal.js
@@ -55,7 +55,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
     //run this function only when password is modified
     if (!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 8)
+    this.password =await bcrypt.hash(this.password, 10)
     next()
 })
 
